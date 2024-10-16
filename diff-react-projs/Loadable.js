0c079ffbbaede5c731ad0359e54c31d5735c35fa
serdevser34 <serdevser34@gmail.com>
2024-10-16 10:49:13 +0300

    Delete diff-react-projs/Loadable.js

--- diff-react-projs/Loadable.js
+++
@@ -1,7 +0,0 @@
-import loadable from 'utils/loadable';
-import LoadingIndicator from 'components/LoadingIndicator';
-import React from 'react';
-
-export default loadable(() => import('./index'), {
-  fallback: <LoadingIndicator />,
-});
